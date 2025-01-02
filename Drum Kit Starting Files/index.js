f638feb6f9fc2aca8c90e28051c33a031c65ba5f
samuel faith <adelajafaithsamuel@gmail.com>
2025-01-02 13:31:45 +0100

    final commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,15 +1,42 @@
+// selects all buttons
+
 var allButtons = document.querySelectorAll(".drum");
+
+
+// ieterate and check for clicks
 
 for (var i = 0; i < allButtons.length; i++) {
 
     allButtons[i].addEventListener("click", switchSounds);
 }
 
+
+// makes sound for each button clicked
+
 function switchSounds () {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+}
+
+
+//detects keyboard press
+
+document.addEventListener("keydown", function(event) {
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+
+// function to make sound
+
+function makeSound(key) {
+    switch (key) {
 
         case "w":
             var crash = new Audio("./sounds/crash.mp3");
@@ -48,4 +75,18 @@ function switchSounds () {
 
         default: console.log(buttonInnerHTML);
     }
+}
+
+// fucntion to animate buttons when pressed or clicked
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    // waits for about 0.1s to retuen to the original style
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
