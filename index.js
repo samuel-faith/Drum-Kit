466dab1bcafbb0447a79ba734062f6e291285671
samuel faith <adelajafaithsamuel@gmail.com>
2025-01-06 20:01:31 +0100

    Final update

--- index.js
+++ index.js
@@ -39,37 +39,37 @@ function makeSound(key) {
     switch (key) {
 
         case "w":
-            var crash = new Audio("crash.mp3");
+            var crash = new Audio("./sound/crash.mp3");
             crash.play();
         break;
 
         case "a":
-            var kickBass = new Audio("kick-bass.mp3");
+            var kickBass = new Audio("./sound/kick-bass.mp3");
             kickBass.play();
         break;
 
         case "s":
-            var snare = new Audio("snare.mp3");
+            var snare = new Audio("./sound/snare.mp3");
             snare.play();
         break;
 
         case "d":
-            var tom1 = new Audio("tom-1.mp3");
+            var tom1 = new Audio("./sound/tom-1.mp3");
             tom1.play();
         break;
 
         case "j":
-            var tom2 = new Audio("tom-2.mp3");
+            var tom2 = new Audio("./sound/tom-2.mp3");
             tom2.play();
         break;
 
         case "k":
-            var tom3 = new Audio("tom-3.mp3");
+            var tom3 = new Audio("./sound/tom-3.mp3");
             tom3.play();
         break;
 
         case "l":
-            var tom4 = new Audio("tom-4.mp3");
+            var tom4 = new Audio("./sound/tom-4.mp3");
             tom4.play();
         break;
 
